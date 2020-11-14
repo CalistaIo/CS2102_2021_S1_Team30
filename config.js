27a5e2218376c6a8162f54b4c340a9a378afa8d2
Nikhilalalalala <uvnikhi@gmail.com>
2020-11-14 18:43:00 +0800

    Add salary

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 const postgres_details = {
-	host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: 'shnu8*shmiom',
-    database: 'project' 
+	
 }
     
 module.exports = postgres_details
